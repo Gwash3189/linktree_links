eecbbbe70400a122814aa99182480693dc156963
Adam Beck <adamb3189@gmail.com>
2020-04-17 13:57:35 +1000

    chore: lint

--- src/components/hello/index.tsx
+++ src/components/hello/index.tsx
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react'
 import styles from './index.module.css'
-import SpotifyIcon from './../svgs/spotify.svg'
+import SpotifyIcon from '../svgs/spotify.svg'
 
 export interface HelloProps { name: string }
 export const greeting = (name: string) => `Hello, ${name}`
-export const Hello = ({ name }: HelloProps) => {
-  return (
+export const Hello = ({ name }: HelloProps) => (
     <Fragment>
       <div className={styles.name}>{greeting(name)}</div>
       <SpotifyIcon />
     </Fragment>
-  )
-}
+)
